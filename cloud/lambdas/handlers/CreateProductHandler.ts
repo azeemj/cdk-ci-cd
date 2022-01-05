89153040054b4c420859fb0c63badb3a5a611853
Azeem Jai <azeem@ayra.com.au>
2022-01-05 19:19:29 +0530

    db issues

--- cloud/lambdas/handlers/CreateProductHandler.ts
+++ cloud/lambdas/handlers/CreateProductHandler.ts
@@ -1,11 +1,11 @@
 import {APIGatewayEvent} from 'aws-lambda';
 import {constants} from 'http2';
 import {JSONParse} from "../utils/JSONParse";
-import {UserInputData} from "../models/UserInputData";
+import {ProductInputData} from "../models/ProductInputData";
 import CloudProductManager from '../CloudProductManager';
 
 export const handler = async (event: APIGatewayEvent) => {
-    const userInputDto = JSONParse<UserInputData>(event.body);
+    const userInputDto = JSONParse<ProductInputData>(event.body);
     const createdUser = await CloudProductManager.create(userInputDto)
     return {
         statusCode: constants.HTTP_STATUS_CREATED,
